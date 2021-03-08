dbc4238514af976bf221da88c4747d8cd4827057
Andrea Habib <andrea.atef@yahoo.com>
2021-03-08 07:11:06 -0500

    DEV: added database

--- view/php/config.php
+++ view/php/config.php
@@ -4,19 +4,14 @@ if(!isset($_SESSION))
 { 
     session_start(); 
 } 
-DEFINE('SERVER', 'csidatabase'); //server name
-DEFINE('USER', 'habib'); //user
-DEFINE('PASS', 'andrea3383'); //pass
-DEFINE('DATABASE', 'habib_'); //database name
-
-//check if database doesn't return any errors, catches any exception
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); 
-try{
-    $conn = new mysqli(SERVER, USER, PASS, DATABASE);
-    $conn->set_charset("utf8mb4");
-} catch(Execption $e){
-    error_log($e->getMessage());
-    exit("Error connecting to database.");
-}
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
